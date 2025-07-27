97163b1d0fbe855c2e06d7c0ef6921971b2d47cb
Likhith32 <likhithmanakala@gmail.com>
2025-07-27 20:39:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -529,10 +529,11 @@ Your Website Team
 
 
 
-# -------------------- Initial DB Seeder --------------------
+# -------------------- Initial DB Seeder -------------------
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))  # Render provides PORT
-    app.run(host='0.0.0.0', port=port)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
     with app.app_context():
         db.create_all()
         if not Admin.query.first():
